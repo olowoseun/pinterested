db806e8ed7ea4a08e5b6fb298cb1e99acc5a66d4
Olowogoke Seun <seunolowogoke@gmail.com>
2015-01-02 17:51:59 +0100

    Added Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Pinterested::Application.routes.draw do
+  devise_for :users
   root 'home#index'
   get 'home/about'
   # The priority is based upon order of creation: first created -> highest priority.
